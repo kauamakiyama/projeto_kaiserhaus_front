841aa833ba8f7de76aeda6dd1c27cc87c7ae8f63
joaobaungartner <joaopbaung@gmail.com>
2025-10-01 14:26:26 -0300

    Home

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,34 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import './App.css'
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <div className="app">
         <Header />
         <main className="main-content">
           <Routes>
-            <Route path="/" element={
-              <>
-                <div className="hero-section">
-                  <h1>Bem-vindo à KaizerHaus</h1>
-                  <p>O melhor da culinária alemã em São Paulo</p>
-                </div>
-                <Footer />
-              </>
-            } />
+            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </Router>
-  )
+  );
 }
-
-export default App
